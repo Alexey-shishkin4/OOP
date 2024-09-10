e22dc39924870a0daee6c8209dbec994afc97e6b
zxc_ghoul <AlexisV.Shishkin@gmail.com>
2024-09-10 21:05:22 +0700

    edited script

--- Task_1_1_1/j.sh
+++ Task_1_1_1/j.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # Устанавливаем переменные для путей
-SRC=src/main/java/org.example/HeapSort.java
+SRC_DIR="src/main/java"
+BIN_DIR="build/classes/java/main"
+JAVADOC_DIR="build/docs"
+SRC_FILE="org/example/HeapSort.java"
 
-javac -d gradleless $SRC
-javadoc -d gradleless $SRC
-java -classpath gradleless org.example.HeapSort
+
+mkdir -p "$BIN_DIR"
+mkdir -p "$JAVADOC_DIR"
+
+
+javac -d "$BIN_DIR" "$SRC_DIR/$SRC_FILE"
+javadoc -d "$JAVADOC_DIR" "$SRC_DIR/$SRC_FILE"
+java -cp "$BIN_DIR" org.example.HeapSort
