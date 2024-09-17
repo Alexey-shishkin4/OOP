30ddfc0fb46b1227e6bf95f7e3194d3c9fb3e537
zxc_ghoul <AlexisV.Shishkin@gmail.com>
2024-09-17 23:28:32 +0700

    fixed

--- Task_1_1_2/src/test/java/GameTest.java
+++ Task_1_1_2/src/test/java/GameTest.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
