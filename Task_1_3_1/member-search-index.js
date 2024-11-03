3b8c0f6b2955169806591814b845a5d5a0efcb52
Alexey-shishkin4 <Alexey-shishkin4@users.noreply.github.com>
2024-11-03 09:08:43 +0000

    Deploying to gh-pages from @ Alexey-shishkin4/OOP@e2c5c71d26627ece615dd09789a87514914f0fbd 🚀

---
+++ Task_1_3_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.example","c":"SubstringSearch","l":"find(String, String)","url":"find(java.lang.String,java.lang.String)"},{"p":"org.example","c":"SubstringSearch","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"org.example","c":"SubstringSearch","l":"SubstringSearch()","url":"%3Cinit%3E()"}]
