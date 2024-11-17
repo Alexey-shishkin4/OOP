ff2465be5ac21696adb914a253e4896c78687682
Alexey-shishkin4 <Alexey-shishkin4@users.noreply.github.com>
2024-11-17 16:28:08 +0000

    Deploying to gh-pages from @ Alexey-shishkin4/OOP@34ce3c4e7b1efd2c7282dedf32825b0526567d2b 🚀

--- Task_1_4_1/type-search-index.js
+++ Task_1_4_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"GradeBook.Grade"},{"p":"org.example","l":"GradeBook"},{"p":"org.example","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"GradeBook.Grade"},{"p":"org.example","l":"GradeBook"}]
